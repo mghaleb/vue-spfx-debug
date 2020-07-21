1df1b953de0d54c9e1484ba3f8bc842f52ff0d96
mghaleb <1282376+mghaleb@users.noreply.github.com>
2020-07-21 10:11:25 +0100

    initial commit

---
+++ src/webparts/vue/VueWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+  } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from 'VueWebPartStrings';
+
+// Importing Vue.js
+import Vue from 'vue';
+// Importing Vue.js SFC
+import VueComponent from './components/Vue.vue';
+
+export interface IVueWebPartProps {
+  description: string;
+}
+
+export default class VueWebPart extends BaseClientSideWebPart<IVueWebPartProps> {
+
+  public render(): void {
+    const id: string = `wp-${this.instanceId}`;
+    this.domElement.innerHTML = `<div id="${id}"></div>`;
+
+    let el = new Vue({
+      el: `#${id}`,
+      render: h => h(VueComponent, {
+        props: {
+          description: this.properties.description
+        }
+      })
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/vue/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVueWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'VueWebPartStrings' {
+  const strings: IVueWebPartStrings;
+  export = strings;
+}
